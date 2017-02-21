26aa619910ad8489f161a90aae1367d0bfdf599d
alexwolfwang <alexwolfwang@gmail.com>
2017-02-21 17:23:25 -0500

    uploads

--- js.js
+++ js.js
@@ -2,6 +2,19 @@
  * Created by Alex.W on 2017/2/17.
  */
 $(function() {
+
+    //active changing function
+
+    $('.nav-tabs li').on('click', function() {
+        if(!$(this).hasClass('active')) {
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+        }
+
+    });
+
+
+    //checkbox's mark color changing function
     $('.save').each(function(){
         $(this).hide().after('<div class="saveCheckbox" />');
 
@@ -11,10 +24,15 @@ $(function() {
         $(this).toggleClass('checked').prev().prop('checked',$(this).is('.checked'))
     });
 
+
+    //email validation function
     function validateEmail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
+
+
+    //email box add style function
 
     var emailM = [];
 
