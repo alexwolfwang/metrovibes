99dfd28250cdb282d1c35e94816a889e6792d782
alexwolfwang <alexwolfwang@gmail.com>
2017-02-21 12:26:14 -0500

    upgrades

--- js.js
+++ js.js
@@ -2,5 +2,13 @@
  * Created by Alex.W on 2017/2/17.
  */
 $(function() {
+    $('.save').each(function(){
+        $(this).hide().after('<div class="saveCheckbox" />');
+
+    });
+
+    $('.saveCheckbox').on('click',function(){
+        $(this).toggleClass('checked').prev().prop('checked',$(this).is('.checked'))
+    });
 
 });
